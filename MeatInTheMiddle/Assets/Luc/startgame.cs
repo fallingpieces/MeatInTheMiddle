e8ac6c102bb824e0014e208cf713b8f50342c3aa
xhaxhoja <106795248+xhaxhoja@users.noreply.github.com>
2026-03-28 20:10:01 -0400

    added maps and start game

---
+++ MeatInTheMiddle/Assets/Luc/startgame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
