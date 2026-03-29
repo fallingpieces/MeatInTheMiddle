ff8bcdae391ea595865d5df33f47fc6fada683fb
fallingpieces <ryanjohnyouness@gmail.com>
2026-03-29 00:18:05 -0400

    Character frog movement animation with some additions for toad

--- MeatInTheMiddle/Assets/Ryan/Plyr_Mvmt/Frog_Mvmt.cs
+++ MeatInTheMiddle/Assets/Ryan/Plyr_Mvmt/Frog_Mvmt.cs
@@ -4,16 +4,21 @@ public class FrogMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal_P1");
         float y = Input.GetAxisRaw("Vertical_P1");
-        rb.linearVelocity = new Vector2(x, y).normalized * moveSpeed;
+        Vector2 input = new Vector2(x, y).normalized;
+        rb.linearVelocity = input * moveSpeed;
+
+        animator.SetBool("isMoving", input.magnitude > 0);
     }
 }
